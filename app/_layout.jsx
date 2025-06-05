d7c09c3046585645e1b1bd719e5ac858d97ea8ad
Ori Sheiner <ori.sheiner@gmail.com>
2025-06-05 21:35:44 +0300

    helloWorld

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+const Rootlayour = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack />
+    </View>
+  );
+};
+
+export default Rootlayour;
+
+const styles = StyleSheet.create({});
---
+++ constans/Colors.js
@@ -0,0 +1,2 @@
+export const Colors = {};
+//Here you define what are the colors of the  dark theme, light theme ,etc.
