d0a767888cc42bcaacd46aa2dffabcd222b69c48
Ori Sheiner <ori.sheiner@gmail.com>
2025-06-09 13:57:41 +0300

    SaveWork

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import {Header} from "../components/Header";
 
 const Rootlayour = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Stack />
+      </Header>
     </View>
   );
 };
