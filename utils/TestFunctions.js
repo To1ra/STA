6a68e8b74cb8df1056c376c6d1ec783a7eaf2966
Lev <levanabel100@gmail.com>
2025-06-09 13:58:26 +0300

    changes

--- utils/TestFunctions.js
+++ utils/TestFunctions.js
@@ -1,4 +1,4 @@
-//this folder is to test potential function that we will use
+//this file is to test potential function that we will use
 //for example: monthly salary calculator, taxes calculator ect...
 //this file can also be used to test which data structs we should use to keep out data, like shifts.
 
@@ -12,3 +12,4 @@
 //it should create a object or struct that contains all the above data, but also calculated information like: salary for that day, special rates, overtime, overall duration.
 
 //should we use a class or struct?
+
