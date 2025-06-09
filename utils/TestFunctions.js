d10494a5d8504ac29a96b7125b66033236e17ef0
Lev <levanabel100@gmail.com>
2025-06-09 18:49:32 +0300

    Started working on app logic
    need to figure out app boundaries, how modularity we want to add, and how to save variables like rest days.

--- utils/TestFunctions.js
+++ utils/TestFunctions.js
@@ -10,6 +10,27 @@
 //the first building block of our app is the basic shift.
 //the creation function should get: start and end date and time of shift, hourly rate of that shift, transport fees, and optional text. 
 //it should create a object or struct that contains all the above data, but also calculated information like: salary for that day, special rates, overtime, overall duration.
+//because overtime, rest day hours, and pay can change, each shift should also keep the value of the variable at the time the shift was created.
 
 //should we use a class or struct?
 
+
+//new idea, what if user would like to add special rates into specific times of the shift(cant do this in competitor)?
+
+var HourlyRate = 34; // this variables should be global in our code or apart from a larger class.
+var OvertimeStart = 8; 
+var RestDays;
+
+
+class Shift{
+constructor(Start, End, HourlyRate, TransportFees, Text) {
+    this.Start = Start;
+    this.End = End;
+    this.HourlyRate = HourlyRate;
+    this.OvertimeStart = OvertimeStart;
+    this.TransportFees = TransportFees;
+    this.Text = Text;
+    this.RestDays = RestDays;
+  }
+
+}
