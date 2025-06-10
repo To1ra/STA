7b2394341128634bcbe579965e44d440f300e56e
Lev <levanabel100@gmail.com>
2025-06-10 16:26:43 +0300

    starting time logic

--- utils/TestFunctions.js
+++ utils/TestFunctions.js
@@ -18,19 +18,25 @@
 //new idea, what if user would like to add special rates into specific times of the shift(cant do this in competitor)?
 
 var HourlyRate = 34; // this variables should be global in our code or apart from a larger class.
-var OvertimeStart = 8; 
-var RestDays;
+var OvertimeStart = [8,0]; 
+var RestDays = [[5,18,30],[6,18,30]]; //how do we keep reaccuring days and hours for rest days? well we can just keep the days sunday to saturday (0-6) , hours (0-23), and minutes (0-59) , we can easily get the current time of the format by using:
+// const date = new Date(); 
+// day = date.getDay();
+// hour = date.getHours();
+// minute date.getMinutes();
+
 
 
 class Shift{
 constructor(Start, End, HourlyRate, TransportFees, Text) {
-    this.Start = Start;
-    this.End = End;
-    this.HourlyRate = HourlyRate;
-    this.OvertimeStart = OvertimeStart;
-    this.TransportFees = TransportFees;
-    this.Text = Text;
-    this.RestDays = RestDays;
+    this.Start = Start; //date object
+    this.End = End; // date object
+    this.HourlyRate = HourlyRate; //double? 
+    this.OvertimeStart = OvertimeStart; //how many [hours,minutes] to wait until overtime
+    this.TransportFees = TransportFees; 
+    this.Text = Text; //string
+    this.RestDays = RestDays; // 2x3 matrix 
+    this.pay = CalculatePay()
   }
 
 }
---
+++ utils/Time.js
@@ -0,0 +1,44 @@
+//utils for timed durations and timestamps
+
+class Duration{
+    
+    constructor(hours,minutes){
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    DurationToPay(HourlyRate){
+        return HourlyRate*(this.hours + (this.minutes/60))
+    }
+
+
+}
+
+class WeeklyDuration{
+    constructor(start , end){  //2 weekly timestamps
+        this.start = start;
+        this.end = end;
+    }
+ 
+}
+
+class WeeklyTimeStamp{
+    constructor(day,hour,minute){
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+    }
+}
+
+function DateToDuration(start, end){
+  const ms = Math.abs(end - start);
+  const totalSeconds = Math.floor(ms / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  return new Duration(hours,minutes)
+}
+
+/* tests
+dur = new Duration(8,20);
+console.log(dur.DurationToPay(30))
+*/
