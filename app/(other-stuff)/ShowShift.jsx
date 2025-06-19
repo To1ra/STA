a09d99e4a124c760b660d5eef242e72a9fc06cb9
Ori Sheiner <ori.sheiner@gmail.com>
2025-06-19 15:28:14 +0300

    firstCommit

---
+++ app/(other-stuff)/ShowShift.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ShowShift = () => {
+  return (
+    <View>
+      <Text>ShowShift</Text>
+    </View>
+  );
+};
+
+export default ShowShift;
+
+const styles = StyleSheet.create({});
